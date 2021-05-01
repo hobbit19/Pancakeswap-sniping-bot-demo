be8309655d52658183691e5c047931eec9cf8acc
Davidkolh <72505611+aviddot@users.noreply.github.com>
2021-05-01 19:20:37 +0200

    V1 Demo
    Contact me on telegram (@aviddot) if you want the full version.

---
+++ configfile.py
@@ -0,0 +1,23 @@
+import sys
+
+token_address='0'
+decimals='18'
+bnb_amount='0'
+speedms='0'
+sec_wait='20'
+liquidity_decrease='0'
+max_slippage='49'
+gwei='20'
+infura_url='https://bsc-dataseed3.defibit.io/'
+enable_liq_decrease='0'
+enable_waiting='1'
+liq_before='1'
+approvetoken='1'
+
+version='2'
+amount_threads='1'
+my_address = ''
+my_pk = ''
+
+
+sys.path.append(".")
